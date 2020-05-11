735ff4b20806046c4d66fe745dab8f9bca1d5554
Hideaki Tai <hideaki.tai@gmail.com>
2020-05-11 13:27:15 +0900

    use PRINTLN

--- examples/example/example.ino
+++ examples/example/example.ino
@@ -19,21 +19,21 @@ void setup()
   LOG_VERBOSE("this is verbose log");
 
   LOG_SET_LEVEL(DebugLogLevel::WARNING);
-  Serial.println("change log level to WARNING");
+  PRINTLN("change log level to WARNING");
 
   LOG_ERROR("this is error log");
   LOG_WARNING("this is warning log");
   LOG_VERBOSE("this is verbose log");
 
   LOG_SET_LEVEL(DebugLogLevel::ERROR);
-  Serial.println("change log level to ERROR");
+  PRINTLN("change log level to ERROR");
 
   LOG_ERROR("this is error log");
   LOG_WARNING("this is warning log");
   LOG_VERBOSE("this is verbose log");
 
   LOG_SET_LEVEL(DebugLogLevel::NONE);
-  Serial.println("change log level to NONE");
+  PRINTLN("change log level to NONE");
 
   LOG_ERROR("this is error log");
   LOG_WARNING("this is warning log");
@@ -47,6 +47,6 @@ void setup()
 
 void loop()
 {
-  Serial.println("if NDEBUG is commented out (assert is enabled), does not come here");
+  PRINTLN("if NDEBUG is commented out (assert is enabled), does not come here");
   delay(1000);
 }
