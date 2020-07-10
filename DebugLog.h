3762816a622a795da561bfb44d2475f48aecea31
Hideaki Tai <hideaki.tai@gmail.com>
2020-07-10 17:56:06 +0900

    fix compile error for avr etc.

--- DebugLog.h
+++ DebugLog.h
@@ -39,10 +39,32 @@ namespace debug {
     struct Logger
     {
         virtual ~Logger() {}
-        virtual void print(const String&) = 0;
-        virtual void println(const String&) = 0;
-        virtual void println() = 0;
         virtual void flush() = 0;
+
+        virtual size_t print(const __FlashStringHelper*) = 0;
+        virtual size_t print(const String&) = 0;
+        virtual size_t print(const char[]) = 0;
+        virtual size_t print(char) = 0;
+        virtual size_t print(unsigned char, int = DEC) = 0;
+        virtual size_t print(int, int = DEC) = 0;
+        virtual size_t print(unsigned int, int = DEC) = 0;
+        virtual size_t print(long, int = DEC) = 0;
+        virtual size_t print(unsigned long, int = DEC) = 0;
+        virtual size_t print(double, int = 2) = 0;
+        virtual size_t print(const Printable&) = 0;
+
+        virtual size_t println(const __FlashStringHelper*) = 0;
+        virtual size_t println(const String&) = 0;
+        virtual size_t println(const char[]) = 0;
+        virtual size_t println(char) = 0;
+        virtual size_t println(unsigned char, int = DEC) = 0;
+        virtual size_t println(int, int = DEC) = 0;
+        virtual size_t println(unsigned int, int = DEC) = 0;
+        virtual size_t println(long, int = DEC) = 0;
+        virtual size_t println(unsigned long, int = DEC) = 0;
+        virtual size_t println(double, int = 2) = 0;
+        virtual size_t println(const Printable&) = 0;
+        virtual size_t println(void) = 0;
     };
 
     template <typename SdType, typename FileType>
@@ -58,17 +80,40 @@ namespace debug {
         : sd(&sdclass)
         , path(path)
         {
-#ifdef FILE_WRITE
+#ifdef O_WRITE
             file = sd->open(path.c_str(), O_WRITE | O_CREAT);
+#elif defined (FILE_WRITE)
+            file = sd->open(path.c_str(), FILE_WRITE);
 #endif
         }
 
         virtual ~SdLogger() { if (file) file.close(); }
-
-        virtual void print(const String& str) override { file.print(str); }
-        virtual void println(const String& str) override { file.println(str); }
-        virtual void println() override { file.println(); }
         virtual void flush() override { file.flush(); }
+
+        virtual size_t print(const __FlashStringHelper* x) override { return file.print(x); }
+        virtual size_t print(const String& x) override { return file.print(x); }
+        virtual size_t print(const char x[]) override { return file.print(x); }
+        virtual size_t print(char x) override { return file.print(x); }
+        virtual size_t print(unsigned char x, int b = DEC) override { return file.print(x, b); }
+        virtual size_t print(int x, int b = DEC) override { return file.print(x, b); }
+        virtual size_t print(unsigned int x, int b = DEC) override { return file.print(x, b); }
+        virtual size_t print(long x, int b = DEC) override { return file.print(x, b); }
+        virtual size_t print(unsigned long x, int b = DEC) override { return file.print(x, b); }
+        virtual size_t print(double x, int b = 2) override { return file.print(x, b); }
+        virtual size_t print(const Printable& x) override { return file.print(x); }
+
+        virtual size_t println(const __FlashStringHelper* x) override { return file.println(x); }
+        virtual size_t println(const String& x) override { return file.println(x); }
+        virtual size_t println(const char x[]) override { return file.println(x); }
+        virtual size_t println(char x) override { return file.println(x); }
+        virtual size_t println(unsigned char x, int b = DEC) override { return file.println(x, b); }
+        virtual size_t println(int x, int b = DEC) override { return file.println(x, b); }
+        virtual size_t println(unsigned int x, int b = DEC) override { return file.println(x, b); }
+        virtual size_t println(long x, int b = DEC) override { return file.println(x, b); }
+        virtual size_t println(unsigned long x, int b = DEC) override { return file.println(x, b); }
+        virtual size_t println(double x, int b = 2) override { return file.println(x, b); }
+        virtual size_t println(const Printable& x) override { return file.println(x); }
+        virtual size_t println(void) override { return file.println(); }
     };
 
     Logger* logger {nullptr};
